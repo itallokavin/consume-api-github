0341b4eb49d526ef21bc87fc1807fd9dd0f4fde3
Itallo Kavin <italo.keven50@gmail.com>
2023-02-02 16:09:33 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react'
 import axios from 'axios'
+import style from './home.module.css'
 
 export default function Home() {
   
@@ -10,22 +11,26 @@ export default function Home() {
 
   const [valor,setValor] = useState(valorInicial)
   
-  function handleChange(e){
+  let [nome, setNome] = useState('')
+  let [company, setCompany] = useState('')
+  let [avatar, setAvatar] = useState('')
+
+  function handleChange(e:any){
     const {name,value} = e.target;
     setValor({...valor,[name]:value});
   }
 
   const router = useRouter()
 
-  function handleSubmit(e){
+  function handleSubmit(e:any){
     e.preventDefault();
     axios
     .get(`https://api.github.com/users/`+ valor.username)
 
     .then((resp)=>{
-      console.log('Deu certo')
-      const login = resp.data.name
-      console.log(login)
+      setNome(resp.data.name)
+      setCompany(resp.data.bio)
+      setAvatar(resp.data.avatar_url)
     })
     .catch(()=>{
       console.log('Deu erro.')
@@ -34,22 +39,38 @@ export default function Home() {
   }
   
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input 
-            type="text" 
-            placeholder='username' 
-            name='username'
-            id='username'
-            onChange={handleChange}
-          />
-          <input type="submit" value="Enviar" />
-      </form>
+    <>
+      <div className={style.campReq}>
+        <div className={style.title}>
+          <h2>Informe um usuário do github: </h2>
+        </div>
+        <div className={style.input}>
+            <form onSubmit={handleSubmit}>
+              <input 
+                type="text" 
+                placeholder='username' 
+                name='username'
+                id='username'
+                onChange={handleChange}
+              />
+              <input type="submit" value="Buscar" className={style.send} />
+            </form>
+          </div>
+      </div>
 
-      <input id='result' />
-
-    </div>
-
-    
+      <div className={style.campRes}>
+        <div className={style.card}>
+          <div className={style.cardHeader}>
+            <div className={style.headerImg}>
+              <img src={avatar}/>
+            </div>
+            <div className={style.headerTitle}>
+              <h3>{nome}</h3>
+              <p>{company}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </>
   )
 }
